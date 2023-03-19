de7f635ab73bd7e402661e6fbdfb3e0e7695f179
Hiwot Alemayehu <99314935+hiwiR@users.noreply.github.com>
2023-03-18 21:13:23 -0400

    resume component done

--- portofolio/src/app/sharedcomponents/landing/landing.component.ts
+++ portofolio/src/app/sharedcomponents/landing/landing.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-
+  data: any = [
+    {
+      imagesrc : "https://sloboda-studio.com/wp-content/uploads/2018/04/1.png",
+      linkto : "frontend",
+      title : "Front Application development",
+      description : "Front Application development Front Application development Front Application development"
+  },
+  {
+    imagesrc : "https://sloboda-studio.com/wp-content/uploads/2018/04/1.png",
+    linkto : "frontend",
+    title : "Front Application development",
+    description : "Front Application development Front Application development Front Application development"
+},
+{
+  imagesrc : "https://sloboda-studio.com/wp-content/uploads/2018/04/1.png",
+  linkto : "frontend",
+  title : "Front Application development",
+  description : "Front Application development Front Application development Front Application development"
+}
+]
 }
--- portofolio/src/app/sharedcomponents/shared.module.ts
+++ portofolio/src/app/sharedcomponents/shared.module.ts
@@ -2,17 +2,25 @@ import { NgModule } from "@angular/core";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { CommonModule } from "@angular/common";
 import { LandingComponent } from './landing/landing.component';
+import { CardcomponentComponent } from './cardcomponent/cardcomponent.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 @NgModule({
     declarations: [
         NavbarComponent,
-        LandingComponent
+        LandingComponent,
+        CardcomponentComponent,
+        ResumeComponent,
+        ContactComponent
     ],
     imports: [
         CommonModule
     ],
     exports: [
         NavbarComponent,
-        LandingComponent
+        LandingComponent,
+        CardcomponentComponent,
+        ResumeComponent
     ]
 })
 export class SharedModule{
